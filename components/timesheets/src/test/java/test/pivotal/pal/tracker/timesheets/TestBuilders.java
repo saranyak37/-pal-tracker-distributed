bfbdd1f52843de59dfa25906f2a5487bd52879a1
saranyak37 <saranya.k37@wipro.com>
2019-07-05 12:56:51 -0700

    chnaged perm2

--- components/timesheets/src/test/java/test/pivotal/pal/tracker/timesheets/TestBuilders.java
+++ components/timesheets/src/test/java/test/pivotal/pal/tracker/timesheets/TestBuilders.java
@@ -16,36 +16,36 @@ public class TestBuilders {
 
     public static TimeEntryRecord.Builder testTimeEntryRecordBuilder() {
         return timeEntryRecordBuilder()
-            .id(11)
-            .projectId(12)
-            .userId(13)
-            .date(LocalDate.parse("2017-09-19"))
-            .hours(20);
+                .id(11)
+                .projectId(12)
+                .userId(13)
+                .date(LocalDate.parse("2017-09-19"))
+                .hours(20);
     }
 
     public static TimeEntryFields.Builder testTimeEntryFieldsBuilder() {
         return timeEntryFieldsBuilder()
-            .projectId(12)
-            .userId(13)
-            .date(LocalDate.parse("2017-09-19"))
-            .hours(20);
+                .projectId(12)
+                .userId(13)
+                .date(LocalDate.parse("2017-09-19"))
+                .hours(20);
     }
 
     public static TimeEntryForm.Builder testTimeEntryFormBuilder() {
         return timeEntryFormBuilder()
-            .projectId(12)
-            .userId(13)
-            .date("2017-09-19")
-            .hours(20);
+                .projectId(12)
+                .userId(13)
+                .date("2017-09-19")
+                .hours(20);
     }
 
     public static TimeEntryInfo.Builder testTimeEntryInfoBuilder() {
         return timeEntryInfoBuilder()
-            .id(11)
-            .projectId(12)
-            .userId(13)
-            .date("2017-09-19")
-            .hours(20)
-            .info("time entry info");
+                .id(11)
+                .projectId(12)
+                .userId(13)
+                .date("2017-09-19")
+                .hours(20)
+                .info("time entry info");
     }
 }
